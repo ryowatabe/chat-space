db5c056163724d56b7a75f41b71042334c0324d1
ryowatabe <42095725+ryowatabe@users.noreply.github.com>
2019-05-31 10:22:39 +0900

    gem-devise-setting
    # WHAT
    ユーザ機能の設計実装を行なった
    
    #WHY
    ログイン、ログアウトの実装のため

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
   devise_for :users
-  resources :users, only: [:new, :create, :edit, :update]
-  resources :messeages, only: [:index, :update]
-  resources :groups, only: [:show, :new, :create, :edit] do
-    resources :messeages, only: [:new, :create]
+  resources :users, only: [:index,:edit, :update] 
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messeages, only: [:index, :create]
   end
 
-  root 'messeages#index'
+  root 'groups#index'
 end
