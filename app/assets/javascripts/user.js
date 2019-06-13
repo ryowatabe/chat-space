01af8c27b7e563cd73a512c37a3af657713a475a
ryowatabe <42095725+ryowatabe@users.noreply.github.com>
2019-06-13 21:25:20 +0900

    インクリメンタルサーチの実装
    # WHAT
    インクリメンタルサーチの細い実装を行なった。
    追加されたユーザは再び検索結果に出ないように反映
    # WHY
    細かいところの実装が必要なため

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,21 +1,27 @@
 $(function(){
+  var chatMembers = [];
+  $("input[name='group[user_ids][]']").each(function(i){
+    var chatMember = $("input[name='group[user_ids][]']").eq(i).val();
+    chatMembers.push(chatMember);
+  });
+
   function appendUser(user){
     var html =`<div class="chat-group-user clearfix id="chat-group-user-${user.id}">
                 <p class="chat-group-user__name">${user.name}</p>
                 <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
               </div>`
-    $('.chat-group-form__search.clearfix').append(html)
+    $("#user-search-result").append(html)
   }
 
   function appendErrMsgToHTML(msg){
     var html =`<div class="chat-group-user clearfix">
                  <p class="chat-group-user__name">${msg}</p>
                </div>`
-    $('.chat-group-form__search.clearfix').append(html)
+    $("#user-search-result").append(html)
   }
 
   function addAppendUser(user){
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${user.userId}'>
+    var html = `<div class='chat-group-user clearfix' id='chat-group-user-${user.userId}'>
                   <input name='group[user_ids][]' type='hidden' value='${user.userId}'>
                   <p class='chat-group-user__name'>${user.userName}</p>
                   <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
@@ -36,10 +42,19 @@ $(function(){
     })
 
     .done(function(users){
-      $(".chat-group-user.clearfix").remove();
+      $("#user-search-result").empty();
       if(users.length !== 0){
         users.forEach(function(user){
-          appendUser(user);
+          for(var i = 0; i < chatMembers.length; i++){
+            var flg = 0;
+            if(user.id == chatMembers[i]){
+              break;
+            }
+            if(i < chatMembers.length-1){
+              continue;
+            }
+            appendUser(user);
+          }
         });
       }
       else{
@@ -58,10 +73,22 @@ $(function(){
       user = $(this).data();
       addAppendUser(user);
       $(this).parent().remove();
+      chatMembers =[];
+      $("input[name='group[user_ids][]']").each(function(i){
+        var chatMember = $("input[name='group[user_ids][]']").eq(i).val();
+        chatMembers.push(chatMember);
+      });
+      console.log(chatMembers);
     });
 
     $(document).on("click",'.user-search-remove.chat-group-user__btn.chat-group-user__btn--remove.js-remove-btn',function(){
       $(this).parent().remove();
+      chatMembers =[];
+      $("input[name='group[user_ids][]']").each(function(i){
+        var chatMember = $("input[name='group[user_ids][]']").eq(i).val();
+        chatMembers.push(chatMember);
+      });
+      console.log(chatMembers);
     });
   })
 });
